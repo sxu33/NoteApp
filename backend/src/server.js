7d1427922ab7898cf9767ce2f3ebddb29bb19764
sxu33 <sxu3@academic.rrc.ca>
2025-11-17 11:04:08 -0600

    prepared for deplayment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from "cors"
 import dotenv from "dotenv"
+import path from "path"
 import noteRoutes from "./routes/notesRoutes.js"
 import {connectDB} from "./config/db.js"
 
@@ -12,18 +13,24 @@ dotenv.config()
 
 const app = express();
 const PORT = process.env.PORT || 5001
-
-
+const __dirname = path.resolve()
+if(process.env.NODE_ENV !== "production"){
+    app.use(cors(
+        {origin: "http://localhost:5173"}
+    ))
+}
 
 
 //middleware
 app.use(express.json())
-app.use(cors(
-    {origin: "http://localhost:5173"}
-))
+
 app.use(rateLimiter)
 app.use("/api/notes", noteRoutes)
 
+if(process.env.NODE_ENV === "production"){app.use(express.static(path.join(__dirname, "../frontend/dist")))
+app.get("*", (req,res)=>{
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+})}
 
 
 connectDB().then(()=> app.listen(5001, ()=>{
